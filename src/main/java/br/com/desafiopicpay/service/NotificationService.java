e51a9246f446fda512cd1bccd8c04a388245ccf3
rbqueiroz <30379824+rbqueiroz@users.noreply.github.com>
2024-04-28 17:35:34 -0300

    update variaveis notification and transaction

--- src/main/java/br/com/desafiopicpay/service/NotificationService.java
+++ src/main/java/br/com/desafiopicpay/service/NotificationService.java
@@ -1,6 +1,7 @@
 package br.com.desafiopicpay.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,16 @@ public class NotificationService {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Value("${status.notification}")
+    private String statusNotification;
+	
 	public void sendNotification(Usuario usuario, String message) throws Exception {
 		
 		String email = usuario.getEmail();
 		
 		NotificationDTO notificationRequest = new NotificationDTO(email, message);
 		
-		ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
+		ResponseEntity<String> notificationResponse = restTemplate.postForEntity(statusNotification, notificationRequest, String.class);
 		
 		if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
 			System.out.println("Erro ao enviar notificação");
--- src/main/java/br/com/desafiopicpay/service/TransactionService.java
+++ src/main/java/br/com/desafiopicpay/service/TransactionService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,9 @@ public class TransactionService {
 	
 	@Autowired
 	private NotificationService notificationService;
+	
+	@Value("${status.transaction}")
+    private String statusTransaction;
 	
 	
 	public Transaction createTransaction(TransactionDTO transaction) throws Exception {
@@ -66,7 +70,7 @@ public class TransactionService {
 	}
 	
 	public boolean authorizeTransaction(Usuario remetente, BigDecimal valor) {
-		ResponseEntity<Map> authorizationResponse  = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+		ResponseEntity<Map> authorizationResponse  = restTemplate.getForEntity(statusTransaction, Map.class);
 		
 		if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
 			String message  = (String) authorizationResponse.getBody().get("message");
